e445e3484b986fff3f60d9b1d66470bc3b275ea1
MARKLEWIS MUTUGI <164312183+lewiii254@users.noreply.github.com>
2025-04-25 20:06:28 +0400

    diabetes

--- diabetes_predictor.py
+++ diabetes_predictor.py
@@ -32,6 +32,7 @@ y = df['Outcome']
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
+
 # Split Data
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
